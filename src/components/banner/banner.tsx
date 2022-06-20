7ce161ab91deb6aef28e44e45570b324321fff94
Kimberly Blandon <kimberly.blandong@gmail.com>
2022-06-20 20:19:16 -0300

    added banner

---
+++ src/components/banner/banner.tsx
@@ -0,0 +1,33 @@
+import { Typography, useMediaQuery } from '@mui/material'
+import { useTheme } from '@mui/material/styles'
+import headphones from '../../assets/slider/headphones.png'
+
+import React from 'react'
+import { Bannercontainer, BannerContent, BannerDescription, BannerImage, BannerTitle } from '../../styles/banner'
+import { Colors } from '../../styles/theme'
+
+const Banner: React.FC = (): JSX.Element => {
+    const theme = useTheme()
+    const matches = useMediaQuery(theme.breakpoints.down('md'))
+
+    return (
+        <Bannercontainer>
+            <BannerImage src={headphones} />
+            <BannerContent>
+                <Typography variant='h6' color={Colors.secondary}>
+                    Collection
+                </Typography>
+                <BannerTitle variant='h2'>
+                Sounds like the perfect deal
+                </BannerTitle>
+                <BannerDescription variant='subtitle1'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Ipsam reiciendis commodi pariatur ducimus impedit error fugit dolores sed quisquam, 
+                    reprehenderit amet deleniti vitae quaerat animi dolorum provident eveniet qui! Itaque.
+                </BannerDescription>
+            </BannerContent>
+        </Bannercontainer>
+    )
+}
+
+export default Banner
