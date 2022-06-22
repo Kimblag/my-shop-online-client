cd8b221ab7057376e966094ee4996c0f3d0bd96c
Kimberly Blandon <kimberly.blandong@gmail.com>
2022-06-22 18:59:19 -0300

    product detail done

---
+++ src/components/productDetail/ProductDetail.tsx
@@ -0,0 +1,92 @@
+import { Box, Button, Dialog, DialogContent, DialogTitle, IconButton, Slide, Typography, useMediaQuery, useTheme } from '@mui/material'
+import React from 'react'
+import { Colors } from '../../styles/theme'
+import CloseIcon from '@mui/icons-material/Close'
+import ReactDOM from 'react-dom'
+import { Product, ProductDetailInfoWrapper, ProductDetailWrapper, ProductImage } from '../../styles/products'
+import ButtonIncDec from '../ui/ButtonIncDec'
+import FavoriteIcon from '@mui/icons-material/Favorite'
+import { Facebook, Instagram, Twitter } from '@mui/icons-material'
+
+export interface IProps {
+    open: boolean
+    onClose: () => void
+    product: any
+}
+interface TransitionProps {
+    children: React.ReactElement<any, any>;
+}
+
+const Transition = React.forwardRef((props: TransitionProps, ref) => (
+    <Slide direction="down" ref={ref} {...props} />
+));
+
+const ProductDetail: React.FC<IProps> = ({ open, onClose, product }): JSX.Element | null => {
+    const theme = useTheme()
+    const matches = useMediaQuery(theme.breakpoints.down('md'))
+    const modal = (
+        <Dialog
+            TransitionComponent={Transition}
+            open={open}
+            fullScreen
+        >
+            <DialogTitle
+                sx={{
+                    background: Colors.secondary,
+                }}
+            >
+                <Box display="flex" alignItems="center" justifyContent="space-between">
+                    {product.name}
+                    <IconButton onClick={onClose}>
+                        <CloseIcon />
+                    </IconButton>
+                </Box>
+            </DialogTitle>
+            <ProductDetailWrapper flexDirection={matches ? 'column' : 'row'}>
+                <Product sx={{ mr: 8, width: '40%' }}>
+                    <ProductImage src={product.image} />
+                </Product>
+                <ProductDetailInfoWrapper>
+                    <Typography variant='subtitle1'>Availability: {product.stock} in stock</Typography>
+                    <Typography variant='h4' sx={{ lineHeight: 2 }}>{product.name}</Typography>
+                    <Typography>${product.price}</Typography>
+                    <Typography variant='body1'>
+                        {product.description}
+                        {product.description}
+                        {product.description}
+                    </Typography>
+                    <Box
+                        sx={{
+                            mt: 4
+                        }}
+                        display='flex'
+                        alignItems='center'
+                        justifyContent='space-between'
+                    >
+
+                        <ButtonIncDec />
+                        <Button variant='contained'>Add to cart</Button>
+                    </Box>
+                    <Box
+                        display='flex'
+                        alignItems='center'
+                        sx={{ mt: 4, color: Colors.primary }}
+                    >
+                        <FavoriteIcon sx={{ mr: 2 }} />
+                        Add to wishlist
+                    </Box>
+                    <Box sx={{ mt: 4, color: Colors.primary }}>
+                        <Facebook />
+                        <Twitter sx={{ pl: theme.spacing(4) }} />
+                        <Instagram sx={{ pl: theme.spacing(4) }} />
+                    </Box>
+                </ProductDetailInfoWrapper>
+            </ProductDetailWrapper>
+            <DialogContent>
+            </DialogContent>
+        </Dialog>
+    )
+    return open ? ReactDOM.createPortal(modal, document.body) : null
+}
+
+export default ProductDetail
--- src/components/products/SingleProduct.tsx
+++ src/components/products/SingleProduct.tsx
@@ -5,32 +5,37 @@ import FavoriteIcon from "@mui/icons-material/Favorite"
 import FitScreenIcon from "@mui/icons-material/FitScreen"
 import ShareIcon from "@mui/icons-material/Share"
 import ProductMeta from './ProductMeta'
+import { useDialogModal } from '../../hooks/useDialogModal'
+import ProductDetail from '../productDetail/ProductDetail'
+
 type Props = {
     product?: any
     matches?: boolean
 }
 
 const SingleProduct: React.FC<Props> = ({ product, matches }): JSX.Element => {
+    const { open, toggle } = useDialogModal()
     return (
         <>
-        <Product>
-            <ProductImage src={product.image} alt={product.name} />
-            <ProductMeta product={product} matches={matches} />
-            <ProductActionsWrapper>
-                <Stack direction='row'>
-                    <ProductFavButton isfav={1}>
-                        <FavoriteIcon />
-                    </ProductFavButton>
-                    <ProductActionButton>
-                        <ShareIcon />
-                    </ProductActionButton>
-                    <ProductActionButton>
-                        <FitScreenIcon />
-                    </ProductActionButton>
-                </Stack>
-            </ProductActionsWrapper>
-            <ProductAddToCart variant="contained">Add to Cart</ProductAddToCart>
-        </Product>
+            <Product>
+                <ProductImage src={product.image} alt={product.name} />
+                <ProductMeta product={product} matches={matches} />
+                <ProductActionsWrapper>
+                    <Stack direction='row'>
+                        <ProductFavButton isfav={1}>
+                            <FavoriteIcon />
+                        </ProductFavButton>
+                        <ProductActionButton>
+                            <ShareIcon />
+                        </ProductActionButton>
+                        <ProductActionButton onClick={toggle}>
+                            <FitScreenIcon />
+                        </ProductActionButton>
+                    </Stack>
+                </ProductActionsWrapper>
+                <ProductAddToCart variant="contained">Add to Cart</ProductAddToCart>
+            </Product>
+            <ProductDetail open={open} onClose={toggle} product={product} />
         </>
     )
 }
