326a54199b4e29a3dfec269126defbeb9de4e0f7
Kimberly Blandon <kimberly.blandong@gmail.com>
2022-06-24 10:12:54 -0300

    add to cart fixed

--- src/components/ui/ButtonIncDec.tsx
+++ src/components/ui/ButtonIncDec.tsx
@@ -4,26 +4,45 @@ import { Colors } from '../../styles/theme'
 import { clamp } from './clamp'
 import RemoveIcon from '@mui/icons-material/Remove'
 import AddIcon from '@mui/icons-material/Add'
+import { useAppDispatch, useAppSelector } from '../../redux/hooks'
+import { decrementProduct, incrementProduct } from '../../redux/features/products/products.slice'
+import { ProductDocument } from '../../redux/interfaces/products/product.interface'
 
-const ButtonIncDec: React.FC = (): JSX.Element => {
+interface Props {
+  product: ProductDocument
+}
+
+const ButtonIncDec: React.FC<Props> = ({ product }): JSX.Element => {
   const clampValue = clamp(1, 10)
-  const [value, setValue] = useState<number>(1)
+  const { cart } = useAppSelector(state => state.products)
+  const [value, setValue] = useState<number>(0)
+  const dispatch = useAppDispatch()
+  const countItemsInCart = cart.find(item => item._id === product._id)?.quantity
+    
+  
   return (
     <Box display='flex'>
       <IconButton
         sx={{ borderRadius: 0, background: `${Colors.secondary}` }}
-        onClick={() => setValue(clampValue(value - 1))}>
+        onClick={() => {
+          setValue(clampValue(value - 1))
+          dispatch(decrementProduct(product))
+        }}>
         <RemoveIcon />
       </IconButton>
       <Typography
         variant='h6'
         sx={{ border: `1px solid ${Colors.secondary}`, p: 2 }}
       >
-        {value}
+        {countItemsInCart}
       </Typography>
       <IconButton
         sx={{ borderRadius: 0, background: `${Colors.secondary}` }}
-        onClick={() => setValue(clampValue(value + 1))}>
+        onClick={() => {
+          setValue(clampValue(value + 1))
+          dispatch(incrementProduct(product))
+        }}
+      >
         <AddIcon />
       </IconButton>
     </Box>
--- src/redux/services/products/products.service.ts
+++ src/redux/services/products/products.service.ts
@@ -1,13 +1,22 @@
 import axios from "axios"
-import { IProduct } from "../../interfaces/products/product.interface"
+import { CartItem } from "../../interfaces/cart/cart.interface"
+import { ProductDocument } from "../../interfaces/products/product.interface"
 
 const getProducts = async () => {
-    const response = await axios.get<IProduct[]>(`http://localhost:3001/api/products`)
+    const response = await axios.get<ProductDocument[]>(`http://localhost:3001/api/products`)
     return response
+}
+
+const addProductToCart = async (payload: any) => {
+    console.log(payload)
+    // const response = await axios.post<ProductDocument>(`http://localhost:3001/api/cart`, payload)
+    // return response
+    return payload
 }
 
 const productService = {
     getProducts,
+    addProductToCart,
 }
 
 export default productService
